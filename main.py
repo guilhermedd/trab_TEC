ac25fee6dce00e4ae2059e99838ad517c8b128ae
Guilherme Diel <guiddiel@gmail.com>
2024-04-15 15:39:08 -0300

    first

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+INPUT_FILE = 'input.in'
+OUTPUT_FILE = 'output.out'
+INITIAL_STATE = '0'
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_input_file():
+    with open(INPUT_FILE, 'r') as input_file:
+        file = input_file.read()
+        return file, file.split('\n')[0][-1]
+        # ;S = fita com inicio a esquerda
+        # ;I = fita duplamente infinita
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+tape, model = get_input_file()
