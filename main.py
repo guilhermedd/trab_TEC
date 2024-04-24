79ff560ee12c82c4461fbcced7c8e583527ab75d
Guilherme Diel <guiddiel@gmail.com>
2024-04-24 15:13:26 -0300

    duble infinity

--- main.py
+++ main.py
@@ -1,184 +1,137 @@
-tests = True
-input_file = "tests/sameamount10.txt" if tests else "input.in"
-
-
-def get_input_file(input_file):
-    with open(input_file, "r") as f:
+def get_input_file(in_file):
+    with open(in_file, "r") as f:
         file = f.read()
         return file, file.split("\n")[0][-1]
         # ;S = fita com inicio a esquerda
         # ;I = fita duplamente infinita
 
 
-def initial_jump_one(start_q, return_to):
-    q0, q1, q2, q3, q4 = [f"q{i}" for i in range(start_q, start_q + 5)]
-    # cur_st, cur_symb, new_symb, dire, new_st = line.split()
+def open_file(quant, out, in_file):
+    new_file = ""
+    machine_type = ''
 
-    new_line = f"""q0z _ * r *
-q0z 1 * * q0
-q0z 0 * * q0
+    new_file += initial_jump_one()
 
+    with open(in_file, "r") as f:
+        file = f.read()
+        for line in file.splitlines():
+            if line.startswith(';'):
+                machine_type = line.split(';')[1]
+                continue
+            else:
+                cur_st, cur_sy, new_sy, direc, new_st = line.split(" ")
+                try:
+                    new_file += f"{int(cur_st) + quant} {cur_sy} {new_sy} {direc} {int(new_st) + quant}\n"
+                except ValueError as e:
+                    new_file += f"{int(cur_st) + quant} {cur_sy} {new_sy} {direc} {new_st}\n"
+
+    with open(out, 'w') as f:
+        f.write(new_file)
+
+    return new_file, machine_type
+
+
+def initial_jump_one():
+    return \
+        f"""0 _ * r *
+0 1 * * q0
+0 0 * * q0
 q0 1 * r *
 q0 0 * r *
 q0 _ _ r qz
-
 qz _ _ l qz1
 qz 1 * * q0
 qz 0 * * q0
-
 qz1 _ $ l q1
-
 q1 0 # r q2
 q1 # * l q4z
 q1 1 # r q3
-
 q2 $ 0 l q1
 q2 # 0 l q1
-
 q3 # 1 l q1
 q3 $ 1 l q1
-
-q4 # ! r 0
+q4 # ! r 1
 q4 _ _ r q4
 q4 0 0 * q1
 q4 1 1 * q1
-
-q4z # * * q4
-q4z 1 * * q4
-q4z 0 * * q4""".split("\n")
-    print(new_line)
-    with open('teste.txt', 'w') as f:
-        # for line in new_line:
-        f.write("\n".join(new_line))
-
-
-# tape, model = get_input_file()
-jump_one(10, 1)
-
-"""q0z _ * r *
-q0z 1 * * q0
-q0z 0 * * q0
-
-q0 1 * r *
-q0 0 * r *
-q0 _ _ r qz
-
-qz _ _ l qz1
-qz 1 * * q0
-qz 0 * * q0
-
-qz1 _ $ l q1
-
-q1 0 # r q2
-q1 # * l q4z
-q1 1 # r q3
-
-q2 $ 0 l q1
-q2 # 0 l q1
-
-q3 # 1 l q1
-q3 $ 1 l q1
-
-q4 # ! r 0
-q4 _ _ r q4
-q4 0 0 * q1
-q4 1 1 * q1
-
-q4z _ _ r q5z
 q4z # * * q4
 q4z 1 * * q4
 q4z 0 * * q4
-
-q5z # _ l 1
-
-;---------------
-
-
-0 _ _ l 1
-0 0 0 r 0
-0 1 1 r 0
-0 B B r 0
-0 X X r 0
-1 1 _ r 2
-1 0 _ r 3
-1 _ _ r 4
-2 0 1 l 5
-2 1 1 l 5
-2 _ 1 l 5
-2 B 1 l 5
-2 X 1 l 5
-5 0 0 l 1
-5 1 1 l 1
-5 _ _ l 1
-5 B B l 1
-5 X X l 1
-1 ! * r q10z
-
-;-----------
-
-q10z _ * r *
-q10z 1 * * q10
-q10z 0 * * q10
-
-q10 1 * r *
-q10 0 * r *
-q10 _ $ l q11
-
-q11 0 # r q12
-q11 # * l q14z
-q11 1 # r q13
-
-q12 $ 0 l q11
-q12 # 0 l q11
-
-q13 # 1 l q11
-q13 $ 1 l q11
-
-q14 # _ r 1
-q14 _ _ r q14
-q14 0 0 * q11
-q14 1 1 * q11
-
-q14z _ _ r q15z
-q14z # * * q14
-q14z 1 * * q14
-q14z 0 * * q14
-
-q15z # _ l 1
-
-
-;-----------
-
-3 0 0 l 6
-3 1 0 l 6
-3 _ 0 l 6
-3 B 0 l 6
-3 X 0 l 6
-
-6 0 0 l 1
-6 1 1 l 1
-6 _ _ l 1
-6 B B l 1
-6 X X l 1
-
-4 _ B r 7
-
-8 1 X l 9
-8 0 X l 10
-8 X X l 8
-8 B B r halt-accept
-
-9 1 1 l 9
-9 X X l 9
-9 0 X r 7
-
-10 0 0 l 10
-10 X X l 10
-10 1 X r 7
-
-7 _ _ l 8
-7 0 0 r 7
-7 1 1 r 7
-7 B B r 7
-7 X X r 7
 """
+
+
+def add_blanc(return_to, next_q):
+    q0, q1, q2, q3, q4, q5 = [f"q{i}" for i in range(next_q, next_q + 6)]
+    new_lines = f"""{return_to} ! * r {q0}z
+{q0}z _ * r *
+{q0}z 1 * * {q0}
+{q0}z 0 * * {q0}
+
+{q0} 1 * r *
+{q0} 0 * r *
+{q0} _ $ l {q1}
+
+{q1} 0 # r {q2}
+{q1} # * l {q4}z
+{q1} 1 # r {q3}
+
+{q2} $ 0 l {q1}
+{q2} # 0 l {q1}
+
+{q3} # 1 l {q1}
+{q3} $ 1 l {q1}
+
+{q4} # _ r {return_to}
+{q4} _ _ r {q4}
+{q4} 0 0 * {q1}
+{q4} 1 1 * {q1}
+
+{q4}z _ _ r {q5}
+{q4}z # * * {q4}
+{q4}z 1 * * {q4}
+{q4}z 0 * * {q4}
+
+{q5} # _ l {return_to}\n"""
+    return new_lines
+
+
+def translate_to_s(file, out_file, next_q, changed_states):
+    final_file = ''
+    final_file += file
+
+    try:
+        for line in file.split('\n'):
+            cur_st, cur_sy, new_sy, direct, new_st = line.split()
+
+            if not cur_st in changed_states and not cur_st.startswith('q'):
+                print(cur_st)
+                changed_states.append(cur_st)
+
+                final_file += add_blanc(cur_st, next_q)
+                next_q += 6
+    except:
+        pass
+
+    with open(out_file, 'w') as f:
+        f.write(final_file)
+
+
+def translate_to_i(file):
+    pass
+
+def main(input_file, output_file, next_q, changed_states):
+    file, machine_type = open_file(1, 'testando.txt', input_file)
+
+    if machine_type == 'I':
+        return translate_to_s(file, output_file, next_q, changed_states)
+
+    return translate_to_i(file)
+
+
+if __name__ == '__main__':
+    tests = True
+    input_file = "tests/sameamount10.txt" if tests else "input.in"
+    out_file = "output.out"
+    next_q = 5
+    changed_states = []
+    main(input_file, out_file, next_q, changed_states)
