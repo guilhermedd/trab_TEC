6ae99d172fed9fbeacbce47e66da52fc32ae1e70
Guilherme Diel <guiddiel@gmail.com>
2024-04-29 08:28:12 -0300

    finished

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # trab_TEC
 
-Para rodar, é preciso que o site `https://morphett.info/turing/turing.html` esteja para o modo `semi-infinite tape`
+- Para rodar de I para S, é preciso que o site `https://morphett.info/turing/turing.html` esteja para o modo `semi-infinite tape`
+- Para rodar de S para I, é preciso que o site `https://morphett.info/turing/turing.html` esteja para o modo `semi-infinite tape`
 
+
+# Run:
+
+```
+python3 main.py
+```
--- main.py
+++ main.py
@@ -10,30 +10,34 @@ def open_file(quant, out, in_file):
     new_file = ""
     machine_type = ''
 
-    new_file += initial_jump_one()
+    try:
+        with open(in_file, "r") as f:
+            file = f.read()
+            for line in file.splitlines():
+                if line.startswith(';'):
+                    machine_type = line.split(';')[1]
+                    new_file += initial_jump_one(machine_type)
+                    continue
+                else:
+                    cur_st, cur_sy, new_sy, direc, new_st = line.split(" ")
+                    try:
+                        new_file += f"{int(cur_st) + quant} {cur_sy} {new_sy} {direc} {int(new_st) + quant}\n"
+                    except ValueError as e:
+                        new_file += f"{int(cur_st) + quant} {cur_sy} {new_sy} {direc} {new_st}\n"
 
-    with open(in_file, "r") as f:
-        file = f.read()
-        for line in file.splitlines():
-            if line.startswith(';'):
-                machine_type = line.split(';')[1]
-                continue
-            else:
-                cur_st, cur_sy, new_sy, direc, new_st = line.split(" ")
-                try:
-                    new_file += f"{int(cur_st) + quant} {cur_sy} {new_sy} {direc} {int(new_st) + quant}\n"
-                except ValueError as e:
-                    new_file += f"{int(cur_st) + quant} {cur_sy} {new_sy} {direc} {new_st}\n"
+        with open(out, 'w') as f:
+            f.write(new_file)
 
-    with open(out, 'w') as f:
-        f.write(new_file)
-
-    return new_file, machine_type
+        return new_file, machine_type
+    except FileNotFoundError as e:
+        print(e)
+        return None, None
 
 
-def initial_jump_one():
-    return \
-        f"""0 _ * r *
+def initial_jump_one(machine_type):
+    if machine_type == 'I':
+        return \
+            """0 _ * r *
 0 1 * * q0
 0 0 * * q0
 q0 1 * r *
@@ -57,6 +61,35 @@ q4 1 1 * q1
 q4z # * * q4
 q4z 1 * * q4
 q4z 0 * * q4
+"""
+    return \
+        """0 1 * r 0
+0 0 * r 0
+0 _ $ l q1
+q1 $ * l q1
+q1 # * l q1
+q1 0 # r q2
+q1 1 # r q3
+q1 _ * r q5
+q2 $ * r q2
+q2 # * r q2
+q2 _ 0 l q4
+q2 0 * r q2
+q2 1 * r q2
+q3 $ * r q3
+q3 # * r q3
+q3 _ 1 l q4
+q3 0 * r q3
+q3 1 * r q3
+q4 0 * l q4
+q4 1 * l q4
+q4 $ * l q4
+q4 # * * q1
+q5 # _ r q5
+q5 $ _ r q5
+q5 1 * r q5
+q5 0 * r q5
+q5 _ * l 1
 """
 
 
@@ -99,25 +132,40 @@ def translate_to_s(file, out_file, next_q, changed_states):
     final_file = ''
     final_file += file
 
-    try:
-        for line in file.split('\n'):
+    for line in file.split('\n'):
+        try:
             cur_st, cur_sy, new_sy, direct, new_st = line.split()
 
             if not cur_st in changed_states and not cur_st.startswith('q'):
-                print(cur_st)
                 changed_states.append(cur_st)
 
                 final_file += add_blanc(cur_st, next_q)
                 next_q += 6
-    except:
-        pass
+        except ValueError:
+            final_file += ""
 
     with open(out_file, 'w') as f:
         f.write(final_file)
 
 
-def translate_to_i(file):
-    pass
+def translate_to_i(file, out_file):
+    final_file = ''
+
+    for line in file.split('\n'):
+        try:
+            cur_st, cur_sy, new_sy, direct, new_st = line.split()
+
+            if not cur_st.startswith('0') and not cur_st.startswith('q'):
+                if direct != '*':
+                    direct = 'r' if direct == 'l' else 'l'
+
+            final_file += f"{cur_st} {cur_sy} {new_sy} {direct} {new_st}\n"
+        except ValueError:
+            final_file += f""
+
+    with open(out_file, 'w') as f:
+        f.write(final_file)
+
 
 def main(input_file, output_file, next_q, changed_states):
     file, machine_type = open_file(1, 'testando.txt', input_file)
@@ -125,45 +173,12 @@ def main(input_file, output_file, next_q, changed_states):
     if machine_type == 'I':
         return translate_to_s(file, output_file, next_q, changed_states)
 
-    return translate_to_i(file)
+    return translate_to_i(file, output_file)
 
 
 if __name__ == '__main__':
-    tests = False
-    input_file = "tests/sameamount10.txt" if tests else "input.in"
-    out_file = "output.out"
+    input_file = input('Digite o nome do arquivo de entrada:\n')
+    out_file = input('Digite o nome do arquivo de saída:\n')
     next_q = 5
     changed_states = []
     main(input_file, out_file, next_q, changed_states)
-
-
-"""
-0 1 * r 0
-0 0 * r 0
-0 _ $ l 1
-
-1 $ * l 1
-1 # * l 1
-1 0 # r 2
-1 1 # r 3
-
-2 $ * r 2
-2 # * r 2
-2 _ 0 l 4
-2 0 * r 2
-2 1 * r 2
-
-3 $ * r 3
-3 # * r 3
-3 _ 1 l 4
-3 0 * r 3
-3 1 * r 3
-
-4 0 * l 4
-4 1 * l 4
-4 $ * l 4
-4 # * * 1
-
-
-Inverter
-"""
